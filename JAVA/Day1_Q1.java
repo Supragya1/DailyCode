05426e40ea84b49b0344243511bf72f3ed0dea16
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-07-26 23:47:46 +0530

    Create Day1_Q1.java

---
+++ JAVA/Day1_Q1.java
@@ -0,0 +1,10 @@
+// Q1 Write a program that prompts the user to enter their name and prints "HELLO, <Name>!".
+import java.util.Scanner;
+public class Day1_Q1{
+  public static void main(String []args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter your name");
+    String name = scan.next();
+    System.out.println("HELLO, " + name + "!");
+  }
+}
