f11073329c501432822fe0f6462656abe3f7573e
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-07-29 23:53:39 +0530

    Update Day2_Q2.java

--- JAVA/Day2_Q2.java
+++ JAVA/Day2_Q2.java
@@ -1,3 +1,4 @@
+// Majority Element
 //Q2 Given an array A of N elements. Find the majority element in the array. A majority element in an array A of 
 // size N is an element that appears more than N/2 times in the array.
 import java.util.*;
