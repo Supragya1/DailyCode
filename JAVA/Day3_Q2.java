edd34fe0292601885413454312c6ff8fef21cc18
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-07-29 23:54:47 +0530

    Update Day3_Q2.java

--- JAVA/Day3_Q2.java
+++ JAVA/Day3_Q2.java
@@ -1,4 +1,4 @@
-// Minimum number of jumps
+// Minimum number of jumps (GFG)
 // Q Given an array of N integers arr[] where each element represents the maximum length of the jump that can be made forward from that element. 
 // This means if arr[i] = x, then we can jump any distance y such that y ≤ x.
 // Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot move through that element.
