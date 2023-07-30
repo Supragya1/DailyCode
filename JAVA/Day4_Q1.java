14d1b63ee556fe07b9422aae9f329200ba8f4465
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-07-30 23:41:20 +0530

    Update Day4_Q1.java

--- JAVA/Day4_Q1.java
+++ JAVA/Day4_Q1.java
@@ -1,49 +1,16 @@
-// Buy sell stock
-import java.util.Scanner;
-
-public class BuySellStock_91 {
-    public static void main(String[]args){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the size of array");
-        int n = scan.nextInt();
-        int prices[] = new int[n];
-        System.out.println("Enter the elemenets of array");
-        for(int i =0;i<n;i++){
-            prices[i]= scan.nextInt();
-        }
-       int buy_price =prices[0];
-       int profit=0,max_profit=0;
-       for(int i=0;i<n;i++){
-        if(buy_price<prices[i]){
-            profit=prices[i]-buy_price;
-            max_profit=Math.max(profit,max_profit);
-        }
-        else{
-            buy_price=prices[i];
-        }
-       }
-       System.out.println("Maximum Profit = "+max_profit);
-    }
-}
-
-
-/* time complexity O(N^2)
- public static void main(String[]args){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the size of array");
-        int n = scan.nextInt();
-        int a[] = new int[n];
-        System.out.println("Enter the elemenets of array");
-        for(int i =0;i<n;i++){
-            a[i]= scan.nextInt();
-        }
-        int max= a[1]-a[0];
+// pairsinArray_85
+import java.util.*;
+public class pairsinArray_85 {
+    public static void printpairs(int a[],int n){
+        int count=0;
         for(int i=0;i<n;i++){
-            for(int j= i+1;j<n;j++){
-                if(max<a[j]-a[i]){
-                    max =a[j]-a[i];
-                }
+            for(int j=i+1;j<n;j++){
+                System.out.print("("+a[i]+","+a[j]+") ");
+                count++;
             }
+            System.out.println("");
         }
-        System.out.println("Maximum profit = "+max);
-    } */
+            System.out.println("total pairs = "+count);
+    }
+   
+}
