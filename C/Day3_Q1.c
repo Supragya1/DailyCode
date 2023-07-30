bc0466ba2589e7926cf8c87a7bd4b83d904ecdf5
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-07-30 23:45:35 +0530

    Create Day3_Q1.c

---
+++ C/Day3_Q1.c
@@ -0,0 +1,18 @@
+// Palindrome
+#include<stdio.h>
+int main(){
+ int n,ld,rev=0,x;
+    printf("enter number\n");
+    scanf("%d",&n);
+    x=n;
+    while(n>0){
+        ld = n%10;
+        rev = (rev*10)+ ld;
+        n = n/10;
+    }
+     printf("%d\n",rev);
+    if(x==rev)
+    printf("palindrome");
+    else
+    printf("not palindrome");
+}
