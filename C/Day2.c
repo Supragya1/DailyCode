969ff911d87877aefa347751e3ec308ad80926b4
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-07-30 23:43:32 +0530

    Create Day2.c

---
+++ C/Day2.c
@@ -0,0 +1,28 @@
+// max of 3
+// to find maximum number between 3 numbers
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter 3 numbers\n");
+    scanf("%d %d %d",&a,&b,&c);
+   if(a!=b || b!=c || a!=c)
+   {
+    if(a>b)
+    {
+        if(a>c)
+        printf("a = %d is the largest number",a);
+        else
+         printf("c = %d is the largest number",c);
+    }
+   
+    else
+    if(b>c)
+    printf("b = %d is the largest number",b);
+    else
+    printf("c = %d is the largest number",c);
+   }
+     else
+    printf("all numbers are equal");
+    return 0; 
+}
