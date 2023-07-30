2f548ce8f6b3ec464e14d3a99abfb6cc7f7a62cd
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-07-30 23:56:26 +0530

    Create Day4_Q1.c

---
+++ C/Day4_Q1.c
@@ -0,0 +1,25 @@
+// Check Prime number
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number to check whether its prime or not \n");
+    scanf("%d",&num);
+    if(num ==1|| num==0){
+        printf("Number is neither prime nor composite\n");
+    }
+    int flag =0;
+    for(int i=2;i<=num/2;i++){
+        if(num%i==0){
+            flag =1;
+            break;
+        }
+    }
+
+    if(flag==0){
+    printf("Prime number\n");
+    }
+    else{
+    printf("Composite number\n");
+    }
+}
