7c0a191944c26f7cbcd66dc8b8b219861cf967f7
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-07-30 23:46:14 +0530

    Create Day3_Q2.c

---
+++ C/Day3_Q2.c
@@ -0,0 +1,25 @@
+// palindrome range upto n
+#include<stdio.h>
+int main(){ int range;
+    printf("Enter range\n");
+    scanf("%d",&range);
+        int reverse=0,ld,num,i,count=0;
+      
+          for(i=0;i<=range;i++){
+             num = i;
+                while(num>0){
+                    ld = num%10;
+                    reverse = reverse*10+ld;
+                    num/=10;
+                }
+               
+               if(i==reverse)
+              {  printf("%d ",reverse);
+             count+=1;
+             }
+              reverse =0;
+              
+             
+          }
+            printf("\n%d", count);
+}
