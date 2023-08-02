3b63936ba684f502ac3d26be3168ed1b6d67d9c2
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-02 23:51:27 +0530

    Create Day5_Q1.py

---
+++ Python/Day5_Q1.py
@@ -0,0 +1,11 @@
+'''Nested Lists in Hackerrank'''
+if __name__ == '__main__':
+    marks=[]
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        marks.append([name,score])
+    scr=[x[1] for x in marks]
+    min_li = sorted(set(scr))
+    stud = sorted([y[0] for y in marks if y[1]==min_li[1]])
+    [print(k) for k in stud]
