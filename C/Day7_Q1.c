b1d4782a6df546c1791e0f3c143c3c4c9bc080c6
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-02 22:28:31 +0530

    Create Day7_Q1.c

---
+++ C/Day7_Q1.c
@@ -0,0 +1,28 @@
+// Dimond Pattern
+#include<stdio.h>
+int main(){
+    int rows;
+    printf("Enter number of rows\n");
+    scanf("%d",&rows);
+    for(int i=0;i<rows;i++){\
+      for(int j=0;j<rows-i;j++){
+      printf(" ");
+      }
+      
+      for(int j=0;j<(2*i)+1;j++){
+      printf("*");
+      }
+      printf("\n");
+    }
+
+    for(int i=rows;i>=0;i--){
+      for(int j=0;j<rows-i;j++){
+      printf(" ");
+      }
+      
+      for(int j=0;j<(2*i)+1;j++){
+      printf("*");
+      }
+      printf("\n");
+    }
+}
