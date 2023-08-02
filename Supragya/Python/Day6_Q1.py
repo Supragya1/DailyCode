3f411b15b73ffd3ed79db0e7147cb3afc2440526
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-03 00:40:16 +0530

    Create Day6_Q1.py

---
+++ Supragya/Python/Day6_Q1.py
@@ -0,0 +1,6 @@
+''' Whats Your Name Hackerrank'''
+def print_full_name(first, last):
+    # Write your code here
+    #print("Hello",first, last+"! You just delved into python.")
+    print(f"Hello {first} {last}! You just delved into python.")
+if __name__ == '__main__':
