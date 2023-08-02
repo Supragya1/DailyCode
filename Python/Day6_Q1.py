bb14bd32c4cd930abfc463945fbf09e922ecd40c
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-03 00:39:40 +0530

    Delete Day6_Q1.py

--- Python/Day6_Q1.py
+++
@@ -1,6 +0,0 @@
-''' Whats Your Name Hackerrank'''
-def print_full_name(first, last):
-    # Write your code here
-    #print("Hello",first, last+"! You just delved into python.")
-    print(f"Hello {first} {last}! You just delved into python.")
-if __name__ == '__main__':
