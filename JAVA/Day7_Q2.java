c228081b5446e636723100f12458c8392875f115
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-02 22:23:37 +0530

    Create Day7_Q2.java

---
+++ JAVA/Day7_Q2.java
@@ -0,0 +1,23 @@
+// linearsearchstring_79
+import java.util.*;
+public class linearsearchstring_79 {
+    public static int linearsearch(String menu[],String key){
+        for(int i=0;i<menu.length;i++){
+        if(menu[i]==key)
+        return i;
+        }
+        return -1;
+    }
+    public static void main(String[]args){ 
+        Scanner scan = new Scanner(System.in);
+        String menu[] = {"dosa","chowmeins","burger","pasta","pizza"};
+        String key = scan.next();
+        int index = linearsearch(menu,key);
+        if(index==-1){
+            System.out.println("Index Not Found");
+        }
+        else
+        System.out.println("key is at index "+ index);
+        
+    }
+}
