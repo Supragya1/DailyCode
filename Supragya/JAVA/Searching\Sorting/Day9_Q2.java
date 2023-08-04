a02d1370e53d09ffd32e9f5e26f7f22ddf5301be
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-04 23:10:18 +0530

    Create Day9_Q2.java

---
+++ Supragya/JAVA/Searching\Sorting/Day9_Q2.java
@@ -0,0 +1,39 @@
+// Selectionsort_93
+// pick the smallest from array and swap it in beginning
+// 5 4 1 3 2
+// 1 5 4 3 2
+// 1 2 5 4 3 
+// 1 2 3 5 4
+// 1 2 3 4 5
+import java.util.Scanner;
+
+public class Selectionsort_93 {
+    public static void Selectionsort(int a[]){
+        int n=a.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(a[min]>a[j]){
+                    min =j;
+                }
+            }
+            int temp =a[min];
+                a[min]=a[i];
+                a[i]=temp;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+   public static void main(String []args){
+         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n =scan.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter the elements of array");
+        for(int i=0;i<n;i++){
+            a[i]=scan.nextInt();
+        }
+        Selectionsort(a);
+    } 
+}
