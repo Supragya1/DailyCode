bff1a8252a9393fb59ec0d6adc0a29ab433d8c0c
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-05 20:49:50 +0530

    Create Day8_Q1.py

---
+++ Supragya/Python/Day8_Q1.py
@@ -0,0 +1,14 @@
+''' sWAP cASE Hackerrank'''
+def swap_case(s):
+    res = ''
+    for ch in s:
+        if ch.isupper():
+            res += ch.lower()
+        else:
+            res += ch.upper()
+    return res
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
