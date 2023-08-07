74d3847f318a8d211499c15f6a9cf3682707a8b8
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-07 21:47:57 +0530

    Create Day10_Q1.py

---
+++ Supragya/Python/Day10_Q1.py
@@ -0,0 +1,10 @@
+''' Text Wrap Hackerrank '''
+import textwrap
+
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
