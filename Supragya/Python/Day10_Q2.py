a2cb9fbe1e248c33e50f5dfee94475854f3b2f0a
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-07 21:53:16 +0530

    Create Day10_Q2.py

---
+++ Supragya/Python/Day10_Q2.py
@@ -0,0 +1,9 @@
+''' Designer Door Mat Hackerrank '''
+n, m = map(int,input().split())
+for i in range(n//2):
+    j = int((2*i)+1)
+    print(('.|.'*j).center(m, '-'))
+print('WELCOME'.center(m,'-'))
+for i in reversed(range(n//2)):
+    j = int((2*i)+1)
+    print(('.|.'*j).center(m, '-'))
