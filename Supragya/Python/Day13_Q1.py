df8767f3bb59d61f9ed577881f791858184fd27f
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-10 21:50:07 +0530

    Create Day13_Q1.py

---
+++ Supragya/Python/Day13_Q1.py
@@ -0,0 +1,8 @@
+''' Introduction to Sets Hackerrank '''
+def average(array):
+    return sum(set(array))/len(set(array))
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
