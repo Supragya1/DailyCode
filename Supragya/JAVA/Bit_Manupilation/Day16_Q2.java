a9c27d5b1cf71196fb33acd54b780caad2b4fdc2
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-11 21:22:45 +0530

    Create Day16_Q2.java

---
+++ Supragya/JAVA/Bit_Manupilation/Day16_Q2.java
@@ -0,0 +1,15 @@
+// SetBit_117
+// set means 1
+import java.util.Scanner;
+
+public class SetBit_117 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int a = scan.nextInt();
+        System.out.println("Enter which bit you wanna Set");
+        int n = scan.nextInt();
+        a=a|(1<<n);
+        System.out.println("Number after setting = "+a);
+    }
+}
