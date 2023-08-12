fc115f9cb38d1ffd2787fa3d7900acb3606eadd1
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-12 23:37:36 +0530

    Create Day15_Q2.py

---
+++ Supragya/Python/Day15_Q2.py
@@ -0,0 +1,11 @@
+''' itertools.permutations() Hackerrank''' 
+from itertools import permutations
+
+s,n=input().split()
+n=int(n)
+vals=list(permutations(s,n))
+res=[]
+for x in vals:
+    res.append(''.join(x))
+
+print('\n'.join(sorted(res)))
