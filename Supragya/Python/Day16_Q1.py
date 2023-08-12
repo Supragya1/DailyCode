5e1879347c7315534b02ab68a3b2d5b802700401
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-13 00:58:44 +0530

    Create Day16_Q1.py

---
+++ Supragya/Python/Day16_Q1.py
@@ -0,0 +1,5 @@
+''' Calendar Module Hackerrank '''
+import calendar
+month, day, year = list(map(int,input().split()))
+ans = calendar.weekday(year, month, day)
+print((calendar.day_name[ans]).upper())
