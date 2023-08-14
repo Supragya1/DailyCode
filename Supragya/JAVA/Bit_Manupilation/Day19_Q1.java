825a708b4a376686c9bdbe12f67f13cf6837e173
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-14 23:03:13 +0530

    Create Day19_Q1.java

---
+++ Supragya/JAVA/Bit_Manupilation/Day19_Q1.java
@@ -0,0 +1,16 @@
+// PowerofTwo_122 
+import java.util.Scanner;
+
+public class PowerofTwo_122 {
+    public static void main(String[]args){
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Enter the number to check");
+        int a = scan.nextInt();
+        if((a&(a-1))==0){
+            System.out.println("Yes, Number is power of 2");
+        }
+        else{
+             System.out.println("Number is not power of 2");
+        }
+    }
+}
