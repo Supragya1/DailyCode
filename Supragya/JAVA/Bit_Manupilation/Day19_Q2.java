a19b2505d2f092ee7ce8658b80a35582e9e905ee
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-14 23:03:51 +0530

    Create Day19_Q2.java

---
+++ Supragya/JAVA/Bit_Manupilation/Day19_Q2.java
@@ -0,0 +1,18 @@
+// CountSetbit_123
+import java.util.Scanner;
+
+public class CountSetbit_123 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number to check");
+        int a= scan.nextInt();
+        int count=0;
+        while(a>0){
+            if((a&1)!=0){
+                count++;
+            }
+            a=a>>1;
+        }
+        System.out.println("Number of set bits = "+count);
+    }
+}
