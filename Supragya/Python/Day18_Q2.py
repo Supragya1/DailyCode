fa975c2461cf0b63afaf835d3045620fd374f8c3
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-15 22:22:01 +0530

    Create Day18_Q2.py

---
+++ Supragya/Python/Day18_Q2.py
@@ -0,0 +1,19 @@
+'''Set .discard(), .remove() & .pop() Hackerrank'''
+n = int(input())
+s = set(map(int, input().split()))
+N = int(input())
+for i in range(N):
+    cmd = list(map(str, input().split()))
+    if cmd[0] == "pop":
+        s.pop()
+    elif cmd[0] == "remove":
+        try:
+            s.remove(int(cmd[1]))
+        except:
+            continue
+    elif cmd[0] == "discard":
+        try:
+            s.discard(int(cmd[1]))
+        except:
+            continue
+print(sum(s))
