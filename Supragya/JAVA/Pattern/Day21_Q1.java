26e48952e0cf09194d058be2656acbc0d2c6077a
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-16 23:20:27 +0530

    Create Day21_Q1.java

---
+++ Supragya/JAVA/Pattern/Day21_Q1.java
@@ -0,0 +1,16 @@
+// starpatter1_49
+import java.util.*;
+public class starpatter1_49 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+            int n = scan.nextInt();
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+ 
