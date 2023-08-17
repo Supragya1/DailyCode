309ebdde1b9587985c25676a3740abcbb0c23d33
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-17 22:24:59 +0530

    Create Day22_Q1.java

---
+++ Supragya/JAVA/Pattern/Day22_Q1.java
@@ -0,0 +1,15 @@
+//  halfpyramid_51
+import java.util.*;
+public class halfpyramid_51 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i=1;i<=n;i++){
+
+            for(int j =1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.print("\n");
+        }
+    }
+}
