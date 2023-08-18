eb07e72c7b1484e31c0aa8c7dd45d2a86bcfcfd3
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-18 23:29:46 +0530

    Create Day23_Q1.java

---
+++ Supragya/JAVA/Pattern/Day23_Q1.java
@@ -0,0 +1,23 @@
+// hollowrectangle_64
+import java.util.*;
+public class hollowrectangle_64 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of rows");
+        int rows = scan.nextInt();
+        System.out.println("Enter number of columns");
+        int columns = scan.nextInt();
+
+        for(int i =1;i<=rows;i++){
+
+            for(int j=1;j<=columns;j++){
+             if(i==1||i==rows||j==1||j==columns){
+                System.out.print("*");
+             }
+             else
+             System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
