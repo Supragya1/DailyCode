a6c5e852d067186b10eae035ebad0180091662be
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-18 23:19:18 +0530

    Create Day23_Q1.c

---
+++ Supragya/C/DSA/Searcing & Sorting/Day23_Q1.c
@@ -0,0 +1,29 @@
+// Selection Sort
+#include<stdio.h>
+int main(){
+    int size,x,min=0;
+    printf("Enter the size of array\n");
+    scanf("%d",&size);
+    int array[size];
+    printf("Enter the elements of array\n");
+    for(int i=0;i<size;i++){
+        scanf("%d",&array[i]);
+    }
+    int temp;
+    for(int i=0;i<size-1;i++){
+        min=i;
+        for(int j=i+1;j<size;j++){
+            if(array[j]<array[min]){
+                min=j;
+            }
+        }
+        temp=array[min];
+        array[min]=array[i];
+        array[i]=temp;
+    }
+        printf("Sorted array is \n");
+        for(int i=0;i<size;i++){
+        printf("%d ",array[i]);
+        }
+        printf("\n");
+}
