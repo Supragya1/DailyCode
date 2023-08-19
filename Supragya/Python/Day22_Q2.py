a7c7bc226efd2e8c3b2dc43953701b02cecf3114
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-19 23:02:56 +0530

    Create Day22_Q2.py

---
+++ Supragya/Python/Day22_Q2.py
@@ -0,0 +1,6 @@
+''' Set .symmetric_difference() Operation Hackerrank '''
+n = int(input())
+s1 = set(map(int , input().split()))
+m = int(input())
+s2 = set(map(int ,input().split()))
+print(len(s1.symmetric_difference(s2)))
