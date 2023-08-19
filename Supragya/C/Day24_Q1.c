e243d20c5a977286d4ca3345d7a6e455a3388e06
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-19 22:57:21 +0530

    Create Day24_Q1.c

---
+++ Supragya/C/Day24_Q1.c
@@ -0,0 +1,16 @@
+/*Write a program to count the number of vowels
+in a text*/
+#include<stdio.h>
+#include<ctype.h>
+int main(){
+    char ch[50];
+    int count=0,i;
+    fgets(ch,50,stdin);
+   for( i=0;ch!='\n';i++){
+        // tolower(ch);
+        if(ch[i]=='a'||ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u'){
+            count++;
+        }
+    }
+    printf("%d",count);
+}
