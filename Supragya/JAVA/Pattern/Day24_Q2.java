68d299abc7ed21a4d2971bd5a58a3e52cbbad9d0
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-19 23:00:21 +0530

    Create Day24_Q2.java

---
+++ Supragya/JAVA/Pattern/Day24_Q2.java
@@ -0,0 +1,17 @@
+// FloydsTriangle_67
+import java.util.*;
+public class FloydsTriangle_67 {
+    public static void main(String[]args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter number of rows");
+    int rows = scan.nextInt();
+    for(int k=1, i=1;i<=rows;i++){
+        
+        for(int j=1;j<=i;j++){
+            System.out.print(k+" ");
+            k++;
+        }
+        System.out.println();
+    }
+    }
+}
