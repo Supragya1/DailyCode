0f7cd958c8738c3de0ab1e600b884f492be77fb2
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-20 21:47:48 +0530

    Create Day25_Q2.c

---
+++ Supragya/C/Day25_Q2.c
@@ -0,0 +1,26 @@
+// Q16 Write a program to print the elements of a 2D array.
+
+#include<stdio.h>
+int main(){
+    int n,m;
+    printf("Enter the number of rows\n");
+    scanf("%d",&n);
+    printf("Enter the number of columns\n");
+    scanf("%d",&m);
+    printf("Enter the elements in the array\n");
+    int a[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+           printf("a[%d][%d] = ",i,j);
+           scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("The array is \n");
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+           printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+}
