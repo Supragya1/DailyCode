8ab3cbe48cccd7869bfdfb1be2f6a3f515eef6e4
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-21 22:53:33 +0530

    Create Day24_Q2.py

---
+++ Supragya/Python/Day24_Q2.py
@@ -0,0 +1,11 @@
+''' Validating phone numbers Hackerrank '''
+for i in range(int(input())):
+    f = input()
+    if f[0] in ["7","8","9"] and len(f) == 10:
+        try:
+            g=int(f)
+            print("YES")
+        except ValueError as e:
+            print("NO") 
+    else:
+        print("NO")
