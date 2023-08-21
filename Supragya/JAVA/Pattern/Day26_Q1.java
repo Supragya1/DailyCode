b3c0c90aa84d056b9556eef8277974e26e13ffe5
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-21 22:44:58 +0530

    Create Day26_Q1.java

---
+++ Supragya/JAVA/Pattern/Day26_Q1.java
@@ -0,0 +1,19 @@
+//  solidrhombus_70
+import java.util.*;
+public class solidrhombus_70 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter numer of rows");
+        int rows = scan.nextInt();
+        for(int i =1;i<=rows;i++){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=rows;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
