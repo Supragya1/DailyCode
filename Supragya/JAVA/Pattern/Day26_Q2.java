802dfcb8de83995619e8e003b0cf12f2280a9116
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-21 22:45:58 +0530

    Create Day26_Q2.java

---
+++ Supragya/JAVA/Pattern/Day26_Q2.java
@@ -0,0 +1,22 @@
+// hollowrhombus_71 
+import java.util.*;
+public class hollowrhombus_71 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of rows");
+        int rows= scan.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=rows;j++){
+                if(i==1||i==rows||j==1||j==rows){
+                    System.out.print("*");
+                }
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+    }
+}
+}
