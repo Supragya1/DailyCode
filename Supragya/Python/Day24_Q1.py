f3c55c1a6d20e3c9ae4b83c00261a608905335ac
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-21 22:50:31 +0530

    Create Day24_Q1.py

---
+++ Supragya/Python/Day24_Q1.py
@@ -0,0 +1,9 @@
+''' Any or All Hackerrank '''
+N =  int(input())
+M = input().split()
+positive = all(int(i)>0 for i in M)
+palindromic = any(i == i[::-1] for i in M)
+if positive and palindromic:
+    print("True")
+else:
+    print("False")
