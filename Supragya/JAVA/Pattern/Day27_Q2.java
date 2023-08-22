2e15f8f0c057059e86563e025a04e963b837799a
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-22 10:18:22 +0530

    Create Day27_Q2.java

---
+++ Supragya/JAVA/Pattern/Day27_Q2.java
@@ -0,0 +1,19 @@
+// numberpyramid_73
+import java.util.*;
+public class numberpyramid_73 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of rows");
+        int rows = scan.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                    System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
