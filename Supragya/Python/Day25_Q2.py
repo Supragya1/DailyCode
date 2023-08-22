4c1dda78748de1eef154961306c61af5ce77a3df
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-22 22:54:51 +0530

    Create Day25_Q2.py

---
+++ Supragya/Python/Day25_Q2.py
@@ -0,0 +1,12 @@
+''' Detect Floating Point Number Hackerrank '''
+N = int(input())
+for n in range(N):
+    try:
+        H = False
+        J = input()
+        if J != "0":
+            float(J)
+            H = True
+    except ValueError as e:
+        H = False
+    print(H)
