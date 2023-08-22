56eee0ca75ee06a105c485e256d9b15a730f8587
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-22 10:16:03 +0530

    Create Day27_Q1.c

---
+++ Supragya/C/Day27_Q1.c
@@ -0,0 +1,20 @@
+//Q20 Write a program to read and display a 3 X 3 matrix.
+
+#include<stdio.h>
+int main(){
+    int a[3][3];
+    printf("Enter the elements of the array \n");
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("a[%d][%d] = ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+printf("The matrix is \n");
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+}
