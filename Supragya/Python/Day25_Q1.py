15f52924d5a0798023e902fca197c25f31846e2d
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-22 22:51:05 +0530

    Create Day25_Q1.py

---
+++ Supragya/Python/Day25_Q1.py
@@ -0,0 +1,14 @@
+''' Map and Lambda Function Hackerrank '''
+cube = lambda x:x**3 
+def fibonacci(n):
+    res = []
+    for n in range(0,n):
+        if n < 2:
+            res.append(n)
+        else:
+            res.append(res[(n-2)] + res[(n - 1)])
+    return res
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
