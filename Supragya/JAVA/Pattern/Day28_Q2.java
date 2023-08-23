32d7a29cba27ccf6330c36ffb60b5767f7195c58
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-23 22:49:42 +0530

    Create Day28_Q2.java

---
+++ Supragya/JAVA/Pattern/Day28_Q2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Pattern{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = scan.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=2 * i - 1;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
