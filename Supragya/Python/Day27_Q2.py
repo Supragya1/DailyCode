8de3017660262d8e46d29cb560d8adb9d73a454c
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-24 10:10:54 +0530

    Create Day27_Q2.py

---
+++ Supragya/Python/Day27_Q2.py
@@ -0,0 +1,7 @@
+''' Set .intersection() Operation Hackerrank '''
+N = int(input())
+A = set(map(int, input().split()))
+M = int(input())
+B = set(map(int, input().split()))
+set1 = A&B
+print(len(set1))
