82c5f5ee43162e70e923510f639ea307e85cec0e
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-24 10:03:36 +0530

    Create Day29_Q1.java

---
+++ Supragya/JAVA/Variable/Day29_Q1.java
@@ -0,0 +1,9 @@
+//output_2
+public class output_2{
+    public static void main(String[]args){
+        int a= 20;
+        System.out.println(a);
+        String name = "Supragya" ;
+        System.out.println(name);
+    }
+}
