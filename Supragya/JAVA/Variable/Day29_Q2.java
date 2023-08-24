804cd7f73a1d6d639a8465a969ec3b6df47f14e9
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-24 10:04:25 +0530

    Create Day29_Q2.java

---
+++ Supragya/JAVA/Variable/Day29_Q2.java
@@ -0,0 +1,28 @@
+// output1_3
+public class output1_3{
+    public static void main(String[]args){
+    byte a= 80;
+    System.out.println(a);
+    
+    char b = 'z';
+    System.out.println(b);
+    
+    boolean c = true;
+    System.out.println(c);
+    
+    float d = 5.1365f;   // Note that you should end the value with an "f"
+    System.out.println(d);
+    
+    int e= 23;
+    System.out.println(e);
+    
+    long f= 135413884245132L; // for long integer(long)  
+    System.out.println(f);  // Note that you should end the value with an "L"
+    
+    double g = 321254.32656451204653; // for long float(Double)
+    System.out.println(g); 
+    
+    String h = "Supragya";
+    System.out.println(h); 
+    }
+}
