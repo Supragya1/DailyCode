cb80148f269627ddf265b415ea19469dc41cf021
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-25 23:26:58 +0530

    Create Day30_Q2.java

---
+++ Supragya/JAVA/Variable/Day30_Q2.java
@@ -0,0 +1,19 @@
+// output3_6
+import java.util.*;
+public class output3_6 {
+    public static void main(String[]args){
+    Scanner scan = new Scanner (System.in);
+
+    String firstname = "Supragya";
+    String lastname = "Gandotra";
+    System.out.println(firstname + " " + lastname);
+    //or
+    String fullname = firstname +" " + lastname;
+    System.out.println(fullname);
+    //or
+    System.out.println("my name is " + fullname);
+
+    int a=5,b=5;
+    System.out.println(a+b);
+    }
+}
