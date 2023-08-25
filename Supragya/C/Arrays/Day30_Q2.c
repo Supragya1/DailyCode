26df009ae02a5ad691033f45281de941d109601a
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-25 23:24:51 +0530

    Create Day30_Q2.c

---
+++ Supragya/C/Arrays/Day30_Q2.c
@@ -0,0 +1,26 @@
+//Q13 Write a program to read an array of n numbers and then find the smallest number.
+#include<stdio.h>
+void scan(int a[],int n){
+    for(int i=0;i<n;i++){
+        printf("\na[%d] = ",i);
+        scanf("%d",&a[i]);
+    }
+}
+int smallest(int a[],int n){
+    int min=a[0];
+    for(int i=0;i<n;i++){
+        if(min>a[i+1]){
+            min=a[i+1];
+        }
+    }
+    return min;
+}
+int main(){
+    int n;
+    printf("Enter the size of array\n");
+    scanf("%d",&n);
+   int a[n];
+   scan(a,n);
+int min =smallest(a,n);
+printf("Smallest number is %d",min);
+}
