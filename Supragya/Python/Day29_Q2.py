07723a22ce7b26cef7ae6c9a3a83b79ca59f5dea
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-26 22:55:38 +0530

    Create Day29_Q2.py

---
+++ Supragya/Python/Day29_Q2.py
@@ -0,0 +1,13 @@
+''' Check Strict Superset Hackerrank'''
+setA = set(map(int, input().split()))
+n = int(input())
+
+is_strict_superset = True
+
+for _ in range(n):
+    inputSet = set(map(int, input().split()))
+    if not setA.issuperset(inputSet):
+        is_strict_superset = False
+        break
+
+print(is_strict_superset)
