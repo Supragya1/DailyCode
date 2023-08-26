96e10025c7fae19664f199df462e4d6dea8bc37a
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-26 22:48:57 +0530

    Create Day31_Q2.java

---
+++ Supragya/JAVA/Variable/Day31_Q2.java
@@ -0,0 +1,21 @@
+// product_8
+import java.util.*;
+public class product_8 {
+    public static void main(String[]args){
+    Scanner scan = new Scanner (System.in);
+
+    System.out.println("enter value of a");
+    int a = scan.nextInt();
+
+    String declare = "enter value of b";
+    System.out.println(declare);
+    int b= scan.nextInt();
+
+    System.out.println(a*b);
+    //or 
+    int product = a*b;
+    System.out.println(product);
+
+    }
+    
+}
