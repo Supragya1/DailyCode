b02c2f09fb0ac2450b7072304c956a744a6a07c5
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-26 22:47:27 +0530

    Create Day31_Q1.java

---
+++ Supragya/JAVA/Variable/Day31_Q1.java
@@ -0,0 +1,18 @@
+// sum1_7
+import java.util.*;
+public class sum1_7{
+    public static void main(String[]args){
+
+    Scanner scan = new Scanner (System.in);
+    System.out.println("enter value of a ");
+    int a = scan.nextInt();
+    
+    System.out.println("enter value of b");
+    int b = scan.nextInt();
+
+    System.out.println(a+b);
+    //or
+    int sum = a+b;
+    System.out.println(sum);
+    }
+}
