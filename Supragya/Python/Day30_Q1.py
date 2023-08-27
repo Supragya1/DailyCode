3971fa9637301b4cd0d28428e9bd42f832df7da4
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-27 22:40:28 +0530

    Create Day30_Q1.py

---
+++ Supragya/Python/Day30_Q1.py
@@ -0,0 +1,10 @@
+''' Arrays Hackerrank '''
+import numpy 
+def arrays(arr):
+    arr.reverse()
+    b = numpy.array(arr,float)
+    return b
+    
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
