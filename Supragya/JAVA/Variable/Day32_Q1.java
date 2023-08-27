9c52ec469ff7370620afa63db28ccd6ee6f93982
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-27 22:37:56 +0530

    Create Day32_Q1.java

---
+++ Supragya/JAVA/Variable/Day32_Q1.java
@@ -0,0 +1,17 @@
+//circlearea_9
+import java.util.*;
+public class circlearea_9{
+public static void main(String[]args){
+
+    Scanner scan =new Scanner(System.in);
+    float pi = 3.14f;
+    System.out.println("enter the value of radius");
+    float r= scan.nextFloat();
+    
+    float area = pi*r*r;
+    System.out.println(area);
+
+    //or
+    System.out.println(pi*r*r);
+}
+}
