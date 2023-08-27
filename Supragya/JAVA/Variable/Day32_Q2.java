26a661326af3ed42fc5ce68cfb3409aee678e89a
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-27 22:38:29 +0530

    Create Day32_Q2.java

---
+++ Supragya/JAVA/Variable/Day32_Q2.java
@@ -0,0 +1,18 @@
+// typeconversion_10
+import java.util.*;  // ALSO KNOWN AS WIDENING CONVERSION OR IMPLICIT CONVERSION
+public class typeconversion_10{
+    public static void main(String[]args){
+     Scanner scan = new Scanner(System.in);
+     int a = scan.nextInt();
+     float b = a;
+     System.out.println(b);
+     //or 
+     float c = scan.nextInt();
+     System.out.println(c);
+    // or
+     int d = 32;
+     float e= d;
+     System.out.println(e);
+
+    }
+}
