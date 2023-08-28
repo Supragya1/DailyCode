9aa7d9803f8031376409ca939fa38143c3da75fe
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-28 16:35:59 +0530

    Create Day31_Q1.py

---
+++ Supragya/Python/Day31_Q1.py
@@ -0,0 +1,11 @@
+''' Concatenate Hackerrank'''
+import numpy
+
+N, M, P = list(map(int, input().split()))
+J = []
+
+for n in range(N+M):
+    l = list(map(int, input().split()))
+    J.append(l)
+    
+print(numpy.array(J))
