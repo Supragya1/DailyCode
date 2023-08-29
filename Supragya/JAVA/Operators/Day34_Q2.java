fbbb7d1c0db296a6010e7dd230e783c5cc9eaf8b
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-29 22:48:10 +0530

    Create Day34_Q2.java

---
+++ Supragya/JAVA/Operators/Day34_Q2.java
@@ -0,0 +1,15 @@
+// unaryoperator_14
+public class unaryoperator_14 {
+    public static void main(String[]args){
+    
+        int a= 10;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+
+        int c = 10;
+        int d =++c;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
