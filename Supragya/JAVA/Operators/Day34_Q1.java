58ddbb59072a6ff4dbbd2618a5a7c4cabe0767e2
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-29 22:47:31 +0530

    Create Day34_Q1.java

---
+++ Supragya/JAVA/Operators/Day34_Q1.java
@@ -0,0 +1,14 @@
+// arithmeticoperator_13
+import java.util.*;
+public class arithmeticoperator_13 {
+    public static void main(String[]args){
+
+    int A = 10;
+    int B = 3;
+    System.out.println(A+B);
+    System.out.println(A-B);
+    System.out.println(A*B);
+    System.out.println(A/B);
+    System.out.println(A%B);
+    }
+}
