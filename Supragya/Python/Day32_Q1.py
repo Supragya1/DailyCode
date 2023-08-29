051c21754c75112ba5e0888d6c9aac0a4ceb4f72
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-29 22:51:44 +0530

    Create Day32_Q1.py

---
+++ Supragya/Python/Day32_Q1.py
@@ -0,0 +1,10 @@
+''' Validating and Parsing Email Addresses Hackerrank'''
+import email.utils
+import re
+
+p = re.compile(r'<[a-z][\w\.-]*@[a-z]+\.[a-z]{1,3}>', re.I)
+
+for i in range(int(input())):
+    lol, ine = input().split()
+    if re.match(p, ine):
+        print(lol, ine)
