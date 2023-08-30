8c6347e1e518a342ec4d275b6b79aa7a6e422da4
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-30 17:55:31 +0530

    Create relationaloperators_15.java

---
+++ Supragya/JAVA/Operators/relationaloperators_15.java
@@ -0,0 +1,15 @@
+// Day35_Q1.java
+public class relationaloperators_15 {
+    public static void main(String[]args){
+   
+        int A =10;
+        int B= 5;
+        System.out.println(A==B);
+        System.out.println(A!=B);
+        System.out.println(A>B);
+        System.out.println(A<B);
+        System.out.println(A>=B);
+        System.out.println(A<=B);
+
+    }
+}
