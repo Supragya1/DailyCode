1b38611e0a3168c18b19eb05246b424f28e47faa
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-08-31 23:34:03 +0530

    Update and rename Day10_Q2.java to Countsort_95.java

--- Supragya/JAVA/Searching\Sorting/Countsort_95.java
+++ Supragya/JAVA/Searching\Sorting/Countsort_95.java
@@ -1,4 +1,5 @@
-// Count sort
+// Day10_Q2.java
+// Countsort_95
 /*Counting sort is a sorting algorithm that sorts the elements of an array by counting
  the number of occurrences of each unique element in the array. The count is stored in
   an auxiliary array and the sorting is done by mapping the count as an index of the auxiliary array */
