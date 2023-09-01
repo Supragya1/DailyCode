a93e5db59bc8d65cf969f037c60a545dffe9174d
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-09-01 22:18:07 +0530

    Create StringConcatenation_103.java

---
+++ Supragya/JAVA/Strings/StringConcatenation_103.java
@@ -0,0 +1,14 @@
+// Day37_Q1.java
+import java.util.Scanner;
+
+public class StringConcatenation_103 {
+     public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        String first_name ="Supragya";
+        String middle_name = "S ";
+        String last_name = "Gandotra";
+        String full_name = first_name+" "+middle_name+last_name;
+        System.out.println(full_name);
+    }
+}
+
