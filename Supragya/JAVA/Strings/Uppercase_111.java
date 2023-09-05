2178d6855d9942aa77b6bd1cc469724927b6ba63
Supragya1 <ssg.gandotra@gmail.com>
2023-09-05 23:45:31 +0530

    Day41_Q1.java

---
+++ Supragya/JAVA/Strings/Uppercase_111.java
@@ -0,0 +1,30 @@
+// Day41_Q1.java
+// Q for a given string convert first letter of each word to uppercase
+//  StringBuilder_name.append(ch) to add ch in last
+//  ch2 =Character.toUpperCase(character);
+//  objectname.toString(); to convert to string int cant be converted but Integer can char cannot but Character can etc
+import java.util.Scanner;
+
+public class Uppecase_111{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str = scan.nextLine();
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+                // ch = str.charAt(i);    ch =Character.toUpperCase(ch);  sb.append(ch);
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        sb.toString();
+        System.out.println(sb);
+    }
+}
