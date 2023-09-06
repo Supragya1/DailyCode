7500b3901911d5515453ca794f94e664c8a49a19
Supragya1 <ssg.gandotra@gmail.com>
2023-09-07 00:02:42 +0530

    Day42_Q2.java

---
+++ Supragya/JAVA/ConditionalStatements/oddeven_20.java
@@ -0,0 +1,15 @@
+// Day43_Q2.java
+// oddeven_20.java
+import java.util.*;
+public class oddeven_20 {
+    public static void main(String[]args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int A = scan.nextInt();
+        if(A%2==0)
+        System.out.println("Number is even");
+        else
+        System.out.println("Number is odd");
+    }
+}
