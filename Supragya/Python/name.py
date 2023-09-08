53c7d405b1ecbaf27c440fd54d54e439fe146e78
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-09-09 03:47:43 +0530

    Update and rename Day1_Q1.py to name.py

--- Supragya/Python/name.py
+++ Supragya/Python/name.py
@@ -1,3 +1,4 @@
+# Day1_Q1.py
 # Q1 Write a Python program that prompts the user to enter 
 # their name and then prints a greeting message in the format "Hello, {Your Name}!"
 name = input("Enter your name ")
