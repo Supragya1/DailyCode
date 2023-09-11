84180d053ba669449e6ef254144d5f86f7eb750f
Supragya1 <ssg.gandotra@gmail.com>
2023-09-11 15:44:10 +0530

    Day46_Q1.java

---
+++ Supragya/JAVA/Loops/whileloop_27.java
@@ -0,0 +1,12 @@
+// Day47_Q1.java
+// whileloop_27.java
+public class whileloop_27 {
+    public static void main(String[]args){
+        int count = 0;
+        while(count<10)
+        {
+            System.out.println("Hello");
+            count++;
+        }
+    }
+}
