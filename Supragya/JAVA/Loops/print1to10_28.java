2c149dc66a7359a07f6dbd111002f44fc2ecfcd1
Supragya1 <ssg.gandotra@gmail.com>
2023-09-11 15:50:26 +0530

    Day47_Q1.java

---
+++ Supragya/JAVA/Loops/print1to10_28.java
@@ -0,0 +1,12 @@
+// Day47_Q2.java
+// print1to_28.java
+public class print1to10_28 {
+    public static void main(String[]args){
+        int i = 1;
+        while(i<=10){
+            System.out.print(i+" ");
+            i++;
+           
+        }
+    }
+}
