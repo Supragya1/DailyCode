197a9d23e93de0548d2de10f8e9e4ae032e2547b
Supragya1 <ssg.gandotra@gmail.com>
2023-09-14 23:33:24 +0530

    Day50_Q2.java

---
+++ Supragya/JAVA/Loops/print1to10_33.java
@@ -0,0 +1,9 @@
+// Day50_Q1.java
+// print1to10_33.java
+public class print1to10_33 {
+    public static void main(String[]arg){
+        int i;
+        for(i=1;i<=10;i++)
+        System.out.print(i+" ");
+    }
+}
