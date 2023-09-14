e6a69d7c0dc51d0a1d244bf967e95f331401030d
Supragya1 <ssg.gandotra@gmail.com>
2023-09-14 23:35:41 +0530

    Day50_Q1.java

---
+++ Supragya/JAVA/Loops/print1toN_34.java
@@ -0,0 +1,14 @@
+// Day50_Q2.java
+// print1toN_34.java
+import java.util.*;
+public class print1toN_34 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the range of numbers n = ");
+        int n = scan.nextInt(),i;
+        for(i=1;i<=n;i++)
+      System.out.print(i+" ");
+    
+    }
+}
+
