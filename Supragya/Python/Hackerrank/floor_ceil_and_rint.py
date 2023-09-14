fc491a124f104d58f0e581492d53cad3298ea080
Supragya1 <ssg.gandotra@gmail.com>
2023-09-14 23:57:59 +0530

    Day35_Q2.py

---
+++ Supragya/Python/Hackerrank/floor_ceil_and_rint.py
@@ -0,0 +1,8 @@
+# Day35_Q2.py
+'''Floor, Ceil and Rint Hackerrank'''
+import numpy
+numpy.set_printoptions(legacy='1.13')
+arr = numpy.array(input().split(),float)
+print(numpy.floor(arr))
+print(numpy.ceil(arr))
+print(numpy.rint(arr))
