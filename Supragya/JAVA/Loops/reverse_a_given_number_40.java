e3ebc7daa9bf3f0e78ec3faf18de5cb79bb81096
Supragya1 <ssg.gandotra@gmail.com>
2023-09-18 04:33:47 +0530

    Day53_Q2.java

---
+++ Supragya/JAVA/Loops/reverse_a_given_number_40.java
@@ -0,0 +1,17 @@
+// Day53_Q2.java
+// 
+import java.util.*;
+public class reverse_a_given_number_40 {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number you want to reverse n = ");
+        int n = scan.nextInt(),reverse = 0;
+        while(n>0){
+            int lastdigit = n%10;
+            reverse = reverse*10+lastdigit;// to save reversed number
+            //System.out.print(lastdigit); to print the reversed number
+            n/=10;
+        }
+        System.out.println(reverse);
+    }
+}
