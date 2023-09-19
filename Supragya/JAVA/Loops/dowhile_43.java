ffbae88cca29b36307140597349bd946e93ae2d9
Supragya1 <ssg.gandotra@gmail.com>
2023-09-19 22:57:44 +0530

    Day55_Q1.java

---
+++ Supragya/JAVA/Loops/dowhile_43.java
@@ -0,0 +1,12 @@
+// Day55_Q1.java
+// dowhile_43.java
+public class dowhile_43 {
+   public static void main(String[]args){
+    int count = 1;
+    do{
+        System.out.println("Hello");
+        count++;
+    }
+    while(count<=10);
+   } 
+}
