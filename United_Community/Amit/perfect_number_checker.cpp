8a7aa6e4f5c0ee904d002d3b2b4dd6aad05081ae
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2023-09-25 01:51:20 +0530

    Update United_Community/Amit/perfect_number_checker.cpp
    Co-authored-by: Supragya <125485615+Supragya1@users.noreply.github.com>

--- United_Community/Amit/perfect_number_checker.cpp
+++ United_Community/Amit/perfect_number_checker.cpp
@@ -2,8 +2,9 @@
 
 using namespace std;
 
-bool isPerfectNumber(int n) {
-  int sum = 1;
+bool isPerfectNumber(long long n) {
+  if(n==1) return 0;
+  long sum = 1;
 
   for (int i = 2; i <=sqrt(n); i++) {
     if (n % i == 0) {
