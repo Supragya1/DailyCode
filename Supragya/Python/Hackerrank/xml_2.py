3f5fe4bedcbde068fcf40831dba0c9c821aa560a
Supragya1 <ssg.gandotra@gmail.com>
2023-10-03 23:51:30 +0530

    Day49_Q2.py

---
+++ Supragya/Python/Hackerrank/xml_2.py
@@ -0,0 +1,21 @@
+# Day49_Q2.py
+'''XML 2 Hackerrank'''
+import xml.etree.ElementTree as etree
+
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    level += 1
+    if level > maxdepth:
+        maxdepth = level
+    for child in elem:
+        depth(child, level)
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
