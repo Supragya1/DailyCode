233a0c857e5d55eff4fd5031f25c0c0c88789554
Supragya1 <ssg.gandotra@gmail.com>
2023-10-07 02:12:34 +0530

    Day53_Q2.py

---
+++ Supragya/Python/Hackerrank/incorrect_regex.py
@@ -0,0 +1,18 @@
+# Day53_Q2.py
+'''Incorrect Regex Hackerrank'''
+import re
+
+def check_regex_validity(s):
+    try:
+        re.compile(s)
+        return True
+        
+    except re.error:
+        return False
+
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    result = check_regex_validity(s)
+    print(result)
