088943c1583de4501b7ee3a1697d9aca424c15c0
Supragya1 <ssg.gandotra@gmail.com>
2023-10-08 03:50:06 +0530

    Day74_Q1.java

---
+++ Supragya/JAVA/Hackerrank/welcome_to_java.java
@@ -0,0 +1,10 @@
+// Day74_Q1.java
+// Welcome to Java! Hackerrank
+package Hackerrank;
+public class welcome_to_java {
+        public static void main(String[] args) {
+            /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+            System.out.println("Hello, World.");
+            System.out.println("Hello, Java.");
+        }
+}
