13654ac36819234e5c346db9196eb8d97a9f83ce
Supragya1 <ssg.gandotra@gmail.com>
2023-10-15 03:45:35 +0530

    Day81_Q2.java

--- Supragya/JAVA/Hackerrank/java_anagrams.java
+++ Supragya/JAVA/Hackerrank/java_anagrams.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class java_anagrams {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
