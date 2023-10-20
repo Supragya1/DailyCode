24c3bb85c674be1d6d111de292affc1961a00c92
Supragya1 <ssg.gandotra@gmail.com>
2023-10-20 23:59:10 +0530

    Day57_Q1.py

---
+++ Supragya/Python/basic.py
@@ -0,0 +1,38 @@
+# Day57_Q1.py
+# Practice code 1
+'''num1 =10
+num2 = 20
+sum1 = num1+num2
+print(sum1)'''
+
+
+# Practice code 2
+'''first_name = "UPES"
+Second_name = "Dehradun"
+concatenate = first_name + Second_name
+print(concatenate)'''
+
+# Practice code 3
+'''num3 = 10
+num4 = 5
+sum2 = num3+num4
+sub =  num3-num4
+mul =  num3*num4
+div =  num3/num4
+print(sum2)
+print(sub)
+print(mul)
+print(div)'''
+
+# Practice code 4
+'''num1 =10
+num2 ="Supragya"
+print(num1)
+print(num2)
+print(num1,num2)'''
+
+# Practice code 5
+'''num1,num2=10,20
+sumnum =  num1+num2
+print(sumnum)'''
+
