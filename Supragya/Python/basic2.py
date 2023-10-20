5043f6320bfeaf78c5814e1c685a220d514c75ee
Supragya1 <ssg.gandotra@gmail.com>
2023-10-21 00:00:05 +0530

    Day57_Q2.py

---
+++ Supragya/Python/basic2.py
@@ -0,0 +1,60 @@
+# Day57_Q2.py
+# Practice code 6
+'''first_name = input("Enter your first name: ")
+Second_name = input("Enter your last name: ")
+name = first_name + " " + Second_name
+print(name)'''
+
+# Practice code 7
+'''num1 = int (input("Enter num1 :"))
+num2 = int (input("Enter num2 :"))
+sumnum = num1+num2
+print(sumnum)'''
+
+# Practice code 8
+'''first_name = input("Enter your first name :")
+Middle_name = input("Enter your Middle name :")
+Last_name = input("Enter your Last name :")
+name = first_name +" "+ Middle_name +" "+ Last_name
+print(name)'''
+
+# Practice code 9
+'''name = input("Enter your name :")
+roll_number = int (input("Enter the roll no :"))
+Sap_id = int (input("Enter the Sap id :"))
+print(name)
+print(roll_number)'''
+
+''' camel case - Each word Except the first start with a capital letter  
+eg myVariableName
+pascal case
+eg MyVariableName
+Snake case
+my_variable_name'''
+
+# Practice code 10
+'''num1=10
+str1="Supragya"
+float=13.0
+print(type(num1))
+print(type(str1))
+print(type(float))'''
+
+#Practice Code 11
+'''name="Supragya"
+NAME="Gandotra"
+num1=10
+NUM1=20
+print(name)
+print(NAME)
+print(num1)
+print(NUM1)'''
+
+#Practice Code 12
+'''name="Supragya"
+num1=10
+name1,name2,num3,num4="Supragya","Gandotra",100,80
+print(name1,name2,num3,num4)
+print(name,num1)'''
+
+print("hello")
