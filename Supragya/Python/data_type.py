c82ccda3065be5d720c069c91ccc7fc59ab8ebe1
Supragya1 <ssg.gandotra@gmail.com>
2023-10-21 23:11:28 +0530

    Day58_Q1.py

---
+++ Supragya/Python/data_type.py
@@ -0,0 +1,12 @@
+# Day58_Q1.py
+'''Write a program to print the data type of the variable having string value and integer value b'''
+a= input("Enter any String \n")
+print(a)
+print( type(a))
+First_name = "Supragya"
+Middle_name = "S"
+Last_name = "Gandotra"
+full_name=  First_name+" "+Middle_name+" "+Last_name
+print(full_name)
+b=5
+print( type(b))
