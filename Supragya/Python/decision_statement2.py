13c2bb7e9ab3202da0d8109c141a110b376063af
Supragya1 <ssg.gandotra@gmail.com>
2023-10-22 23:58:44 +0530

    Day59_Q2.py

---
+++ Supragya/Python/decision_statement2.py
@@ -0,0 +1,220 @@
+'''Name Supragya Gandotra
+Sap id 500107073
+batch 20'''
+#date 25-1-2023
+
+# Practice code 1
+'''num = int(input("Enter the value of num "))
+while(num>0 and num<=5):
+    print(num)
+    num+=1
+else:
+    print("Else block of while loop")'''
+
+# Practice code 2
+'''for i in range(10):
+    print(i,end=" ")
+else:
+    print("Else block of for loop")'''
+
+# Practice code 3
+'''for i in range(0,10,2):
+    print(i)
+    for j in range(0,):
+        print(j ,end = " ")
+    else:
+        print("Inner loop")
+else:
+    print("Outer loop")'''
+
+#Practice code 4
+'''num = int(input("Enter the value of num "))
+while (num<=10):
+    print("\n",num)
+    num +=3
+    pr=0
+    while(pr<=5):
+        print(pr,end=" ")
+        pr+=2
+    else:
+        print("Inner loop")
+else:
+    print("Outer loop")'''
+
+# Practice code 5
+'''num =0
+while(num>=0):
+    print("Hello World")
+    num+=2
+    j=0
+    while(j<5):
+        print(j)
+        if j==2:
+            break
+        j+=1
+    if (num==10):
+        break'''
+
+# Practice code 6
+'''for i in range(0,10,1):
+    print(i)
+    if i==5:
+        break
+else:
+    print("else statement")'''
+
+# Practice code 7
+i=0
+while(i<=10):
+    print(i)
+    i+=1
+    if i==4:
+        continue
+    print(i,"Hello world")
+
+#Practice Code 8: while loop
+'''num = int(input("Enter the value of num:"))
+
+while num>0 and num<=5:
+    print(num)
+    num = num + 1
+else:
+    print("Else block of while loop")'''
+    
+#Practice code 9: for loop with else block
+'''for i in range(10):
+    print(i, end=" ")
+    else:
+        print("Else block of for loop")'''
+
+#Practice code 10:nesting loop
+'''for i in range (0, 10, 2):
+    print(i)
+    for j in range (0, 5):
+        print(j, end=" ")
+    else:
+        print("in inner else")
+else:
+    print("in outer else")'''
+
+#Practice code 11:nesting while loop
+
+'''num = int(input("Enter the value of num:"))
+
+while num<=10:
+    print("\n",num)
+    num=num+3
+    pr=0
+    while pr <=5:
+        print(pr, end= " ")
+        pr+=2
+    else:
+        print("else of inner loop")
+    else:
+        print("else of outer loop")'''
+
+#Practice code 12: break
+'''num=0
+while num>=0:
+    print("Hello world")
+     num+=2
+    if num==10:
+        break'''
+
+#Practice code 13: break
+'''num=0
+while num>=0:
+    print("Hello world")
+    num+=2
+    j=0
+    while j<5:
+         print(j)
+         if j == 2:
+            break
+           j=j+1
+    else:
+        print("In inner else")
+    if num==10:
+        break
+else:
+    print("Inouter else")'''
+
+#Practice code 14: break
+'''for i in range(0, 10, 1):
+    print(i)
+    if i ==5:
+        break
+else:
+    print("pin else statement")'''
+
+#Practice code 15: continue
+'''i = 0
+while i<=10:
+    i+=1
+    if i==4:
+        continue
+    print(i,"Hello world")
+    print(i,"Hello upes")
+else:
+    print("In else block")'''
+
+#Practice code 16: pass
+'''for i in range(10):
+    pass'''
+'''i=0
+while i<=5:
+    pass'''
+'''n=10
+if n>0:
+    pass'''
+
+#Practice Code 17: while loop
+'''num = int(input("Enter the value of num:"))
+
+while num>0 and num<10:
+    print(num)
+    num = num + 2
+else:
+    print("Else block of while loop")'''
+
+#Practice Code 18: while loop
+'''num = int(input("Enter the value of num:"))
+
+while num>0 or num<=5 or num<15:
+    print(num)
+    num = num + 3
+else:
+    print("Else block of while loop")'''
+
+#Practice code 19:nesting loop
+'''for i in range (0, 30, 5):
+    print(i)
+    for j in range (0, 10):
+        print(j, end=" ")
+    else:
+        print("in inner else")
+else:
+    print("in outer else")'''
+
+#Practice code 20: break
+'''num=0
+while num>=0:
+    print("Hello world")
+    num+=5
+    if num==30:
+        break'''
+
+#Practice code 21:nesting while loop
+'''num = int(input("Enter the value of num:"))
+
+while num<=20:
+    print("\n",num)
+    num=num+2
+    pr=0
+    while pr <=10:
+        print(pr, end= " ")
+        pr+=2
+    else:
+        print("else of inner loop")'''
+
+
