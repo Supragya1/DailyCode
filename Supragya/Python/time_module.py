7602cbd1a8ce6548107b51d8b58777567079f3a2
Supragya1 <ssg.gandotra@gmail.com>
2023-11-02 23:21:59 +0530

    Day64_Q1.py

---
+++ Supragya/Python/time_module.py
@@ -0,0 +1,33 @@
+# Day64_Q1.py
+#practice code 42: time module
+
+"""import time
+curr = time.ctime(1627908313.717886)
+print("Current time:", curr)
+
+for i in range(4):
+    time.sleep(1)
+    print(i)
+
+
+obj = time.localtime(1627987508.6496193)
+print(obj)
+
+obj1 = time.gmtime(1627987508.6496193)
+time_sec = time.mktime(obj1)
+print("Local time (in seconds):", time_sec)
+
+obj = time.gmtime(1627987508.6496193)
+print(obj)
+
+from time import gmtime, strftime
+s = strftime("%a, %d %b %Y %H:%M:%S",gmtime(1627987508.6496193))
+print(s)
+
+import time
+obj = time.gmtime(1627987508.6496193)
+time_str = time.asctime(obj)
+print(time_str)
+obj = time.localtime(1627987508.6496193)
+time_str = time.asctime(obj)
+print(time_str)"""
