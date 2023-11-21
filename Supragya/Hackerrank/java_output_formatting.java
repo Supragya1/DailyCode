60a49daa0dd564bca8035021b1553e995ddd9ac2
Supragya1 <ssg.gandotra@gmail.com>
2023-11-22 02:17:09 +0530

    update

--- Supragya/Hackerrank/java_output_formatting.java
+++ Supragya/Hackerrank/java_output_formatting.java
@@ -1,22 +1,20 @@
+// Day76_Q1.java
+// Java Output Formatting Hackerrank
+import java.util.Scanner;
 
-// of java string trim() method 
-import java.io.*; 
+public class java_output_formatting{
 
-// Driver Class 
-class java_output_formatting{ 
-	// Main Function 
-	public static void main (String[] args) { 
-		
-		// Three strings declared 
-		String x="geeks dddddddf deee     ddddddddd   d d  "; 
-		String y="for "; 
-		String z="geeks"; 
-		
-		// Printing without trim function 
-		System.out.println(x+"d"); 
-		
-		x.trim();
-		// Using trim function to get result 
-		System.out.println(x+"d"); 
-	} 
-} 
+    public static void main(String[] args) {
+            Scanner scan=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=scan.next();
+                int x=scan.nextInt();
+                System.out.printf("%-15s%03d\n",s1,x);// here %-15 means left aligned and 15 is the minimum width of the string s for string %03d means minimum 3 digit integer.
+            }
+            System.out.println("================================");
+    }
+}
+
+
+
