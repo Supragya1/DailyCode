e3c7621b3b24c88723585d53a2b7910edf3f908f
Amit S Sahu <Amit.123854@stu.upes.ac.in>
2023-11-25 23:43:23 +0530

    Update United_Community/Amit/Daily-Leetcode/day_3_two_sum.cpp
    Co-authored-by: Supragya <125485615+Supragya1@users.noreply.github.com>

--- United_Community/Amit/Daily-Leetcode/day_3_two_sum.cpp
+++ United_Community/Amit/Daily-Leetcode/day_3_two_sum.cpp
@@ -17,37 +17,37 @@ public:
 };
 
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            int search = target - nums[i];
-            vector<int> nums_copy(nums);
-            sort(nums_copy.begin(), nums_copy.end());
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> ans;
+//         for (int i = 0; i < nums.size(); i++) {
+//             int search = target - nums[i];
+//             vector<int> nums_copy(nums);
+//             sort(nums_copy.begin(), nums_copy.end());
 
-            int low = 0, high = nums.size() - 1;
-            while (low <= high) {
-                int mid = low + (high - low) / 2;
-                if (nums_copy[mid] == search) {
-                    for (int j = 0; j < nums.size(); j++) {
-                        if (nums[j] == search && j != i) {
-                            ans.push_back(i);
-                            ans.push_back(j);
-                            break;
-                        }
-                    }
-                    break;
-                } else if (search > nums_copy[mid]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            }
-            if (!ans.empty()) {
-                break;
-            }
-        }
-        return ans;
-    }
-}; //Works but time complexity too high
+//             int low = 0, high = nums.size() - 1;
+//             while (low <= high) {
+//                 int mid = low + (high - low) / 2;
+//                 if (nums_copy[mid] == search) {
+//                     for (int j = 0; j < nums.size(); j++) {
+//                         if (nums[j] == search && j != i) {
+//                             ans.push_back(i);
+//                             ans.push_back(j);
+//                             break;
+//                         }
+//                     }
+//                     break;
+//                 } else if (search > nums_copy[mid]) {
+//                     low = mid + 1;
+//                 } else {
+//                     high = mid - 1;
+//                 }
+//             }
+//             if (!ans.empty()) {
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+// }; //Works but time complexity too high
 
