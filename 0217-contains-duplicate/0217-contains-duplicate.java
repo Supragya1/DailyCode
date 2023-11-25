ffc8546b1c7c7ce0db3da2dd8e8ec72c8ed85fd7
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-11-26 02:19:34 +0530

    Time: 11 ms (55.80%), Space: 55 MB (67.20%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+// Day105_Q2.java
+// Using Hashset
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
