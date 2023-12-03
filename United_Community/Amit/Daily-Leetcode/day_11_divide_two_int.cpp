15972c099b1d472209fc48e201d697f3cf931ddd
Amit00199 <Amit.123854@stu.upes.ac.in>
2023-12-04 00:16:06 +0530

    Corrected Day-11 Code

--- United_Community/Amit/Daily-Leetcode/day_11_divide_two_int.cpp
+++ United_Community/Amit/Daily-Leetcode/day_11_divide_two_int.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (dividend == INT_MIN && divisor == -1)
-            return INT_MAX;
-        int result = dividend/divisor;
+        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+        if (dividend == INT_MIN && divisor == 1) return INT_MIN;
 
-        return result;
+        long divid = abs(dividend), divis = abs(divisor);
+
+        int quo = 0;
+        while (divis <= divid) {
+            long temp = divis, i = 1;
+            while (temp <= divid - temp) {
+                temp <<= 1;
+                i <<= 1;
+            }
+            quo += i;
+            divid -= temp;
+        }
+
+        if ((dividend < 0) ^ (divisor < 0)) {
+            return -quo;
+        } else {
+            return quo;
+        }
     }
 };
