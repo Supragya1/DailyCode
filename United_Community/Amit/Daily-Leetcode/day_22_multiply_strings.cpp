bf20e69245c614ddc80458acc0413ad201911bdb
Amit00199 <Amit.123854@stu.upes.ac.in>
2023-12-14 21:42:27 +0530

    Day-22

---
+++ United_Community/Amit/Daily-Leetcode/day_22_multiply_strings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") {
+            return "0";
+        }
+
+        int m = num1.size();
+        int n = num2.size();
+        vector<int> result(m + n, 0);
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int sum = result[i + j + 1] + mul;
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        string res;
+        for (int num : result) {
+            if (!(res.empty() && num == 0)) {
+                res.push_back(num + '0');
+            }
+        }
+
+        return res.empty() ? "0" : res;
+    }
+};
