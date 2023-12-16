0005a3623caeffc6511ad4dfd4d1b3218db3cd7b
Mradul-Lakhotiya <142927820+Mradul-Lakhotiya@users.noreply.github.com>
2023-12-16 12:43:06 +0530

    16 Dec 2023(LeetCode)

--- United_Community/Mradul/Daily-Leetcode/day_24_WordSearch.java
+++ United_Community/Mradul/Daily-Leetcode/day_24_WordSearch.java
@@ -2,6 +2,8 @@
 
 
 class Solution {
+    // https://www.notion.so/day_24-f31ec6eaa39743cd85392719ea077176?pvs=4
+    
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
