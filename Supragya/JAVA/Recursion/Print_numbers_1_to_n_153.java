9f747721fe660d5a2308557bb58c637992801327
Supragya1 <ssg.gandotra@gmail.com>
2023-12-21 23:59:32 +0530

    Day105_Q2.java

---
+++ Supragya/JAVA/Recursion/Print_numbers_1_to_n_153.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Print_numbers_1_to_n_153 {
+    public static void print(int n){
+        if(n==0){
+            return;
+        }
+        print(n-1);
+        System.out.print(n+" ");
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=scan.nextInt();
+        print(n);
+    }
+}
