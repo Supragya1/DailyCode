1990c6a4f31978c4b799583fc5c63060987d53e4
Supragya1 <ssg.gandotra@gmail.com>
2024-01-24 23:50:50 +0530

    Day112_Q1.java

--- Supragya/JAVA/Recursion/remove_duplicate_in_string_166.java
+++ Supragya/JAVA/Recursion/remove_duplicate_in_string_166.java
@@ -10,11 +10,22 @@ public class remove_duplicate_in_string_166 {
         }
         else{
             map.put(str.charAt(index), 1);
-            str2.append(str.charAt(index));
+            str2.append(str.charAt(index)); 
             return removeDuplicate(str,map,index+1,str2);
         }
     }
-
+    public static String removeDuplicate2(String str,HashMap<Character,Integer> map,int i){
+        if(i==str.length()){
+            return str;
+        }
+        if(map.containsKey(str.charAt(i))){
+            return removeDuplicate2(str.substring(0, i)+str.substring(i+1, str.length()), map, i+1);
+        }
+        else{
+            map.put(str.charAt(i), 1);
+            return removeDuplicate2(str, map, i+1);
+        }
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the string: ");
@@ -22,5 +33,6 @@ public class remove_duplicate_in_string_166 {
         HashMap<Character, Integer> map = new HashMap<>();
         StringBuilder str2 = new StringBuilder();
         System.out.println("The string after removing duplicate characters is "+removeDuplicate(str, map,0,str2));
+        System.out.println("The string after removing duplicate characters is "+removeDuplicate2(str, map,0));
     }
 }
