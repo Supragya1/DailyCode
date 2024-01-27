1084ec9829db1f0df6de205d638f2b21de7594aa
Supragya1 <ssg.gandotra@gmail.com>
2024-01-28 01:27:42 +0530

    Hackerank

---
+++ Supragya/Hackerrank-ProblemSolving/Solve_Me_First.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solve_Me_First {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println(a+b);
+    }
+}
